3ffc010ada39ad418492adb697c598601a6b72b7
sandeep kumar <sandeep.kumar@trigma.com>
2022-05-31 15:05:23 +0530

    implemented feature2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Feature2Component } from './feature2/feature2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Feature2Component
   ],
   imports: [
     BrowserModule,
