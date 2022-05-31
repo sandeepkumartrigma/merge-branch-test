12c152a5449af2cff251d07c10f91c3fef26b4e9
sandeep kumar <sandeep.kumar@trigma.com>
2022-05-31 15:02:48 +0530

    added feature 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Feature1Component } from './feature1/feature1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Feature1Component
   ],
   imports: [
     BrowserModule,
